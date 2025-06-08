c6c4116ede57661c513debd18a6c54726963d387
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-06-08 15:25:59 +0530

    allow all port

--- hoichoi/settings.py
+++ hoichoi/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://hoichoi-b0ce.onrender.com', '*']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://hoichoi-b0ce.onrender.com",
+    # Add more if needed: e.g. "http://localhost:8000"
+]
 
 
 # Application definition
